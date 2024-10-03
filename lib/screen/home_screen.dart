bb587f4230a78cb11a647dbe8e71a58739344d7e
Alvindo Tri Jatmiko <alvindo56@gmail.com>
2024-10-03 22:42:56 +0700

    feat: detail screen

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+                margin: const EdgeInsets.only(top: 16.0),
+                child: const Text(
+                  'Farm House Lembang',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
