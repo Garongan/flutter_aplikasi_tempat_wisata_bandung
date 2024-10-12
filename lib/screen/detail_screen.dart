4a64cae791cf34790d8da067dd76e5ea76933d89
Alvindo Tri Jatmiko <alvindo56@gmail.com>
2024-10-12 23:55:44 +0700

    fix: ux in action button

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -11,193 +11,218 @@ class DetailScreen extends StatelessWidget {
     final yGap = width * 0.03;
     return Scaffold(
       backgroundColor: const Color.fromRGBO(239, 239, 239, 1),
-      body: Stack(
-        children: <Widget>[
-          ListView(
-            children: <Widget>[
-              Image.asset(
-                'assets/images/farm_house_lembang.jpg',
-                height: 301,
-                fit: BoxFit.cover,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            ListView(
+              padding: EdgeInsets.only(
+                left: xPadding,
+                right: xPadding,
+                top: 100,
               ),
-              Container(
-                margin: EdgeInsets.only(
-                  top: yGap,
-                  left: xPadding,
-                  right: xPadding,
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(7),
+                  child: Image.asset(
+                    'assets/images/farm_house_lembang.jpg',
+                    width: width - (2 * xPadding),
+                    height: 301,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                child: const Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'Farm House Lembang',
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.w500),
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.location_on_outlined,
-                          size: 15,
-                          color: Color.fromRGBO(29, 170, 171, 1),
-                        ),
-                        Text(
-                          'Lembang',
-                          style: TextStyle(
-                              color: Color.fromRGBO(29, 170, 171, 1),
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500),
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(
-                  top: yGap,
-                  left: xPadding,
-                  right: xPadding,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 12, horizontal: 7),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(7),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Icon(
-                              Icons.calendar_today,
-                              color: Color.fromRGBO(29, 170, 171, 1),
-                              size: 24,
-                            ),
-                            SizedBox(
-                              height: yGap,
-                            ),
-                            const Text(
-                              'Open Everyday',
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.w500),
-                            )
-                          ],
-                        ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: yGap,
+                  ),
+                  child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'Farm House Lembang',
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.w500),
                       ),
-                    ),
-                    SizedBox(
-                      width: xGap,
-                    ),
-                    Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 12, horizontal: 7),
-                        decoration: BoxDecoration(
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.location_on_outlined,
+                            size: 15,
+                            color: Color.fromRGBO(29, 170, 171, 1),
+                          ),
+                          Text(
+                            'Lembang',
+                            style: TextStyle(
+                                color: Color.fromRGBO(29, 170, 171, 1),
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: yGap,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.only(
+                            left: 10,
+                            top: 20,
+                            bottom: 20,
+                          ),
+                          decoration: BoxDecoration(
                             color: Colors.white,
-                            borderRadius: BorderRadius.circular(7)),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Icon(
-                              Icons.access_time,
-                              color: Color.fromRGBO(29, 170, 171, 1),
-                              size: 24,
-                            ),
-                            SizedBox(
-                              height: yGap,
-                            ),
-                            const Text(
-                              '09.00 - 20.00',
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.w500),
-                            )
-                          ],
+                            borderRadius: BorderRadius.circular(7),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Icon(
+                                Icons.calendar_today,
+                                color: Color.fromRGBO(29, 170, 171, 1),
+                                size: 24,
+                              ),
+                              SizedBox(
+                                height: yGap,
+                              ),
+                              const Text(
+                                'Open Everyday',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w500),
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(
-                      width: xGap,
-                    ),
-                    Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 12, horizontal: 7),
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(7)),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const Icon(
-                              Icons.monetization_on,
-                              color: Color.fromRGBO(29, 170, 171, 1),
-                              size: 24,
-                            ),
-                            SizedBox(
-                              height: yGap,
-                            ),
-                            const Text(
-                              'Rp. 25.000',
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.w500),
-                            )
-                          ],
+                      SizedBox(
+                        width: xGap,
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.only(
+                            left: 10,
+                            top: 20,
+                            bottom: 20,
+                          ),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(7)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Icon(
+                                Icons.access_time,
+                                color: Color.fromRGBO(29, 170, 171, 1),
+                                size: 24,
+                              ),
+                              SizedBox(
+                                height: yGap,
+                              ),
+                              const Text(
+                                '09.00 - 20.00',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w500),
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                      SizedBox(
+                        width: xGap,
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.only(
+                            left: 10,
+                            top: 20,
+                            bottom: 20,
+                          ),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(7)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Icon(
+                                Icons.monetization_on,
+                                color: Color.fromRGBO(29, 170, 171, 1),
+                                size: 24,
+                              ),
+                              SizedBox(
+                                height: yGap,
+                              ),
+                              const Text(
+                                'Rp. 25.000',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w500),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(
-                  top: yGap,
-                  left: xPadding,
-                  right: xPadding,
+                Container(
+                  margin: EdgeInsets.only(
+                    top: yGap,
+                  ),
+                  child: const Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+                    textAlign: TextAlign.justify,
+                  ),
                 ),
-                child: const Text(
-                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
-                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
-                  textAlign: TextAlign.justify,
+                Gallery(
+                  xPadding: xPadding,
+                  yGap: yGap,
+                  xGap: xGap,
                 ),
-              ),
-              Gallery(
-                xPadding: xPadding,
-                yGap: yGap,
-                xGap: xGap,
-              ),
-            ],
-          ),
-          ActionButton(
-            xPadding: xPadding,
-            xGap: xGap,
-          ),
-        ],
+              ],
+            ),
+            ActionButton(
+              yGap: yGap,
+              xGap: xGap,
+              xPadding: xPadding,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
 class ActionButton extends StatelessWidget {
-  final double xPadding;
+  final double yGap;
   final double xGap;
-  const ActionButton({super.key, required this.xPadding, required this.xGap});
+  final double xPadding;
+  const ActionButton(
+      {super.key,
+      required this.yGap,
+      required this.xGap,
+      required this.xPadding});
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Padding(
-        padding: EdgeInsets.all(xPadding),
+      child: Container(
+        color: const Color.fromRGBO(239, 239, 239, 1),
+        padding: EdgeInsets.symmetric(
+          vertical: yGap,
+          horizontal: xPadding,
+        ),
+        height: 100,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
@@ -282,9 +307,9 @@ class Gallery extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(
-        horizontal: xPadding,
-        vertical: yGap,
+      padding: EdgeInsets.only(
+        top: yGap,
+        bottom: xPadding,
       ),
       child: Column(
         children: [
